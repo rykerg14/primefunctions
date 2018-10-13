1f396d4b34cef90575be4b3249de103e22ecb48f
rykerg14 <43770969+rykerg14@users.noreply.github.com>
2018-10-12 22:52:22 -0700

    UpdateTests

--- testPerformance.js
+++ testPerformance.js
@@ -6,11 +6,11 @@ describe('maxPrimeSum', function () {
   it('maxPrimeSum(10000) should take less than 20ms', function () {
     chai.expect(maxPrimeSum(10000)).to.eql([9521, 65]);
     this.slow(0);
-    this.timeout(200);
+    this.timeout(20);
   });
   it('maxPrimeSum(100000) should take less than 200ms', function () {
     this.slow(0);
     chai.expect(maxPrimeSum(100000)).to.eql([92951, 183]);
-    this.timeout(2000);
+    this.timeout(200);
   });
 });
