8a9ae4c20291cc50e8af5375a7c352c28f9f5851
rykerg14 <43770969+rykerg14@users.noreply.github.com>
2018-10-12 17:58:50 -0700

    cumulativeSum tests complete

--- testCorrectness.js
+++ testCorrectness.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha, chai */
 
-/* global chai, primeGen */
+/* global chai, primeGen, cumulativeSum */
 
 describe('primeGen', function () {
   it('primeGen(10) = [2, 3, 5, 7]', function () {
@@ -8,5 +8,14 @@ describe('primeGen', function () {
   });
   it('primeGen(20) = [2, 3, 5, 7, 11, 13, 17, 19]', function () {
     chai.expect(primeGen(20)).to.eql([2, 3, 5, 7, 11, 13, 17, 19]);
+  });
+});
+
+describe('cumulativeSum', function () {
+  it('cumulativeSum([1, 2, 3, 4]) = [1, 3, 6, 10]', function () {
+    chai.expect(cumulativeSum([1, 2, 3, 4])).to.eql([1, 3, 6, 10]);
+  });
+  it('cumulativeSum([10, 11, 12, 13, 14}) = [10, 21, 33, 46, 60]', function () {
+    chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.eql([10, 21, 33, 46, 60]);
   });
 });
